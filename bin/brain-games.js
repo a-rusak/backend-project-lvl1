3fdc11c196d76334477181df3d42e399156f5eef
a-rusak <alexander.rusak@gmail.com>
2021-01-04 16:58:54 +0300

    Add getuseName

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
 
+// import readlineSync from 'readline-sync';
+import { getUserName } from '../src/cli.js';
+
 console.log('Welcome to the Brain Games!')
+var userName = getUserName();
+console.log('Hello, ' + userName + '!');
